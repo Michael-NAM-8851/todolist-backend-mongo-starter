e69615f8c7df264d64d9fecf91afd24d3a03b4fd
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-15 14:02:49 +0800

    refactor: use findTodo for deletion and update

--- src/main/java/com/afs/todolist/service/TodoService.java
+++ src/main/java/com/afs/todolist/service/TodoService.java
@@ -2,6 +2,7 @@ package com.afs.todolist.service;
 
 import com.afs.todolist.entity.Todo;
 import com.afs.todolist.exception.InvalidIdException;
+import com.afs.todolist.exception.TodoNotFoundException;
 import com.afs.todolist.repository.TodoRepository;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
@@ -26,20 +27,31 @@ public class TodoService {
         return todoRepository.save(todo);
     }
 
+    public Todo findById(String id){
+        Todo todo = todoRepository.findById(id).get();
+        if(todo == null) {
+            throw new TodoNotFoundException(id);
+        }
+        return todo;
+    }
+
     private void validateObjectId(String id){
         if(!ObjectId.isValid(id)){
             throw new InvalidIdException(id);
         }
     }
 
-    public Todo updateTodo(String id, Todo todo) {
+    public Todo updateTodo(String id, Todo updatedTodo) {
         validateObjectId(id);
-        todo.setId(id);
+        Todo todo = findById(id);
+        todo.setText(updatedTodo.getText());
+        todo.setDone(updatedTodo.getDone());
         return todoRepository.save(todo);
     }
 
     public void deleteTodo(String id) {
         validateObjectId(id);
-        todoRepository.deleteById(id);
+        Todo todo = findById(id);
+        todoRepository.delete(todo);
     }
 }
